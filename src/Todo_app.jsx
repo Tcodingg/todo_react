3258bfdf2b88e1177f608a30c26d24bfd5f72f6f
DePrincio <deprincio@DePrincio.local>
2020-12-10 21:45:01 -0500

    input log

--- src/Todo_app.jsx
+++ src/Todo_app.jsx
@@ -1,20 +1,35 @@
 import React, {useState} from "react"
 
 export default function Todo_app(){
-    const [item, setItem] = useState("");
+    const [item, setItem] = useState('');
+    const [itemVal, setItemVal] = useState('')
 
     function handleInput(event){
-        setItem(event.target.value)
-       return console.log(item)
+        const inputVal = event.target.value;
+        setItem(inputVal);
+        setItemVal(inputVal);
+       
     }
+
+    function addItem(){
+        console.log(itemVal)
+        
+    }
+   
+
     return(
         <div className="container">
+            <h1 className='logo'>Todo App</h1>
             <div className="input-container">
                 <input onChange={handleInput} type="text" placeholder='Add Item' value={item}/>
-                <button >ADD</button>
+                <button onClick={addItem}>ADD</button>
             </div>
-            <div className="display">
-                <p></p>
+            <div className="item-display">
+                <p>item </p>
+                <p>item </p>
+                <p>item </p>
+                <p>item </p>
+                <p>item </p>
             </div>
         </div>
     )
